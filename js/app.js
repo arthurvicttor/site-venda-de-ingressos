029356bd345d83e6d4dab2a66184245381340e42
arthurvicttor <arthurvicttorpe@gmail.com>
2024-01-23 10:47:54 -0300

    conflito

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ function comprar(){
         let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
 
         if (quantidade > qtdSuperior) {
-            alert('Quantidade indisponivel para o tipo cadeira Superior');
+            alert('Quantidade indisponivel para o tipo Superior');
         } else {
             qtdSuperior = qtdSuperior - quantidade;
             document.getElementById('qtd-superior').textContent = qtdSuperior;
